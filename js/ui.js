fdabc4d381c55e58d68d70cd1a617ea997401dc1
BuildMateTeam <buildmatebe@gmail.com>
2026-06-29 20:08:05 +0700

    Implement updateStructureInfo function
    Add function to update structure information display

--- js/ui.js
+++ js/ui.js
@@ -72,3 +72,19 @@ function getExtension(filename){
     return filename.substring(index);
 
 }
+// Hiển thị thông tin Structure
+function updateStructureInfo(data){
+
+    document.getElementById("structureSize").textContent =
+        "📐 Size: " +
+        data.width + " × " +
+        data.height + " × " +
+        data.length;
+
+    document.getElementById("blockCount").textContent =
+        "🧱 Blocks: " + data.blocks;
+
+    document.getElementById("paletteCount").textContent =
+        "🎨 Palette: " + data.palette;
+
+}
