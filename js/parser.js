37632c102bbb58fd85942c60687af3d628739fb4
BuildMateTeam <buildmatebe@gmail.com>
2026-06-29 17:35:13 +0700

    Add parseStructure function for .mcstructure files
    Initial implementation of the parseStructure function to read .mcstructure files.

--- js/parser.js
+++ js/parser.js
@@ -1,1 +1,41 @@
+// =====================================
+// BuildMate BE
+// parser.js
+// Version 0.6 Alpha
+// =====================================
 
+/*
+ * Đây là module đọc file .mcstructure
+ * Hiện tại mới là nền móng.
+ * Sau này sẽ đọc NBT thật.
+ */
+
+function parseStructure(file) {
+
+    console.log("================================");
+    console.log("BuildMate BE Parser");
+    console.log("Reading:", file.name);
+    console.log("Size:", file.size, "bytes");
+    console.log("================================");
+
+    return {
+
+        name: file.name,
+
+        size: file.size,
+
+        loaded: true,
+
+        version: "Unknown",
+
+        width: 0,
+
+        height: 0,
+
+        length: 0,
+
+        blocks: 0
+
+    };
+
+}
